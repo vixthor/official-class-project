130b57e18133f4d2e8af0ebb3776a3bba882b7bf
vixthor <86299991+vixthor@users.noreply.github.com>
2022-05-30 14:55:11 +0100

    update

--- pages/signup.php
+++ pages/signup.php
@@ -1,32 +1,11 @@
 <?php require("../layouts/header.php")  ?>
- <?php // $firstName = $email = $password = $date = $lastName =  "";
 
-// if(filter_has_var(INPUT_POST, 'submit')){
-//     // echo "Submitted";
-//     $firstName = $_POST['firstName'];
-//     $lastName = $_POST['lastName'];
-//     $email = $_POST['email'];
-//     $password = $_POST['password'];
-//     $date = $_POST['date'];
-
-//     if(!empty($firstName) && !empty($lastName) && !empty($email) && !empty($password)){
-//         //passed
-//         echo "submited";
-//     }else{
-//         //failed
-//         echo "failed";
-//         $msg = 'Please fill in all fields';
-       
-//     }
-// } ?> 
 <div class="">
-            <form action="" method="post" class=" form_image my-24  mx-auto space-y-6 max-w-3xl p-5 border border-gray-500 rounded-lg">
+            <form action="#" method="post" class=" form_image my-24  mx-auto space-y-6 max-w-3xl p-5 border border-gray-500 rounded-lg">
                 <h2 class="text-3xl text-center pt-3 font-semibold">Sign Up</h2>
                 <p class="text-medium text-gray-600">Fill in the form below to sign in</p>
-            <?php   if($email != ''):    ?>
-                <div class="alert "> <?php  echo $msgclass ?> <?php  echo $msg ?>       </div>
-            <?php  endif;     ?>
-               <div class="flex space-x-3">
+        
+                <div class="flex space-x-3">
                 <div class="w-1/2">
                     <label for="Firstname" class="">First Name</label>
                     <input type="text" name="firstName" class="input" placeholder="Enter your First Name">
